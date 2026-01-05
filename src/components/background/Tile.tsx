36f38af0d213292b5df4297592d3740b25464626
Camleaf <bdaace09@gmail.com>
2026-01-04 19:42:16 -0500

    I'm a fan of this navbar design

--- src/components/background/Tile.tsx
+++ src/components/background/Tile.tsx
@@ -1,7 +1,7 @@
 import "./Background.css"
 
 // The scattered tiles across the background which are filled
-function Tile(props:{colour:string,sidelength:string,x:number,y:number}) {
+function Tile(props:{colour:string,sidelength:string,x:number,y:number, posMode?:string}) {
     
   // tileDimension will be dynamically generated eventually
   const tileDimension = {
@@ -9,6 +9,7 @@ function Tile(props:{colour:string,sidelength:string,x:number,y:number}) {
      "--sidelength": props.sidelength,
      "--x": props.x,
      "--y": props.y,
+     "--posMode": (props.posMode) ? props.posMode : "fixed",
   } as React.CSSProperties;
 
   return (
--- src/components/navbar/Nav.tsx
+++ src/components/navbar/Nav.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { getTileDimensions } from "../background/Background.tile.ts";
 import type { tileDimensionData } from "../background/Background.tile.ts";
+import Tile from "../background/Tile.tsx";
+import { pages, type page } from "./Nav.switch.ts";
 import "./Nav.css";
 
 function Nav() {
@@ -20,7 +22,21 @@ function Nav() {
     return (
         <>
             <div className="navbar-head">
-                       
+                    {(dimensionData) ? pages.map((page:page, index:number)=>( // If dimensionData loaded then display the navbar
+                        // Each navbar item has a colour and name attached. 
+                        <div className="navbar-item" 
+                            key={index} 
+                            style={{"--sidelength":dimensionData.sidelength} as React.CSSProperties}
+                        >
+                            
+                            <Tile colour={page.colour} sidelength={dimensionData.sidelength} x={0} y={0} posMode={"default"}/> 
+                            <h3>{page.name}</h3>
+                        
+
+                        </div>
+                    )) :  // If dimensionData not loaded then return empty
+                        <></>
+                    }
             </div>
         </>
     );
