cb926ade41df1b1b5c7b3c3c876c752f896614f8
Camleaf <bdaace09@gmail.com>
2026-01-05 21:03:52 -0500

    progress save start making page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,16 @@
+import "./Home.css";
 
 // The idea for this page is it's kind of like a terminal. Not much user interaction but in terms of appeareance
 function Home() {
 
   return (
     <>
-        <div className="">
-
+        <div className="home-wrapper">
+           <p>
+            Hi There! Welcome to my website!
+            <br/>
+            Here you'll find 
+           </p> 
         </div>
     </>
   );
