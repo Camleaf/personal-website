2f9e33431a5727f17d220285d91675e246a21f36
Camleaf <bdaace09@gmail.com>
2026-06-18 12:33:41 -0400

    start homepage

---
+++ src/components/landing.tsx
@@ -0,0 +1,20 @@
+
+function Landing() {
+    return (
+            <div className="w-full cont h-full flex flex-col lg:flex-row items-center justify-center">
+                <div className="w-[40rem] h-[40rem] rounded-xl overflow-hidden items-center justify-center">    
+                    <img id="" className="w-[40rem] mt-[-12rem]" src="/hero.png"/>
+                </div>
+                <div className={"box-border w-fit overflow-hidden flex flex-row items-start justify-start"}>
+                    <div className="pt-8 pl-0 lg:pt-0 lg:pl-16 lg:w-auto flex flex-col items-start justify-center">
+                        <h1 className="w-full text-center lg:text-left">Alexander Edwards</h1>
+                        <h2 className="w-full text-center lg:text-left">aka Camleaf</h2>
+                        <p className="w-full text-center lg:text-left">Robotics & STEM</p>
+                    </div>
+                </div>
+                 <div className={`${(window.innerWidth < 1024) ? 'w-[120vw]' : ''}`}></div>
+            </div>
+    )
+}
+
+export default Landing 
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -6,24 +6,34 @@ import {
     Route,
 } from "react-router-dom";
 import Main from "./main";
-
+import Landing from "../components/landing";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
 
 function Layout() {
     const [marginTop, setMarginTop] = useState(0);
     const [width, setWidth] = useState(0)
     const scrollRef:Ref<HTMLDivElement> = useRef<HTMLDivElement>(null);
+    const divRef = useRef<HTMLDivElement>(null);
 
+    const updateMarginTop = () => {
+            if (divRef.current) {
+                const divHeight = divRef.current.clientHeight; // Get the height of the <div>
+                const offset = (window.innerHeight - divHeight) / 2; // Calculate the marginTop
+                setMarginTop(offset);
+            }
+    };
 
     useEffect(() => {
         setWidth(window.innerWidth)
         setWidth(window.innerWidth + (window.innerWidth>=1024 ? 0 : 12*parseFloat(getComputedStyle(document.documentElement).fontSize)))
         window.addEventListener("resize", function(){
-            setMarginTop(window.innerHeight*1.1);
+            updateMarginTop();
             setWidth(window.innerWidth + (window.innerWidth>=1024 ? 0 : 12*parseFloat(getComputedStyle(document.documentElement).fontSize)))
         });
         document.body.style.overflow = 'hidden';
         setTimeout(function(){
-            setMarginTop(window.innerHeight*1.1);
+            updateMarginTop();
             window.scrollTo(0, 0)
             setTimeout(() => {
                 document.body.style.overflow = '';
@@ -38,28 +48,25 @@ function Layout() {
     return (
         <>
         {/* Mechmania logo load + intial screen */}
-            <div className="w-screen h-screen fixed top-0 left-0 loaded z-[100] animate-loaded pointer-events-none">{/*<Load/>*/}</div>
+        
+        {/*<div className="w-screen h-screen fixed top-0 left-0 loaded z-[100] animate-loaded pointer-events-none"><Load/></div>*/}
+            
             <div className="z-[10]">
-            {/*<Nav scrollRef={scrollRef} marginTop={marginTop}/>*/}
+            <Navbar scrollRef={scrollRef} marginTop={marginTop}/>
             </div>
             
-            <div className={`fixed w-full h-full top-0 left-0 flex flex-row items-center align-middle justify-center z-[-1] pointer-events-none`}> 
-            {/*<Initial/>*/}
-            </div>
 
             {/* Initial screen cover, email, + navbar & router */}
             <div style={{ width: width+'px',marginTop: `${marginTop}px` }} className="gap-16 flex flex-col min-h-[120vh] items-center bg-white" ref={scrollRef}>
                
+                <div ref={divRef} className={`w-full h-full top-0 left-0 flex flex-row items-center align-middle justify-center`}> 
+                        <Landing/>
+                </div>
                 <div className="gap-16 flex flex-col items-center justify-start w-full max-h-fit bg-white lg:px-32 box-border z-[0] relative top-0">
-                
-                    <div
-                        style={{
-                            borderColor: "#444",
-                            marginBottom:`1rem`
+                     
 
-                        }}
-                        className="z-50 relative bg-white box-content rounded-[4rem] border-[8px] border-solid h-0 -left-[10vw] w-[120vw]"
-                    />                    
+
+                    
                     {/*
                     <div style={{marginBottom:`4rem`}}>
                         <Line dir down/>
@@ -80,7 +87,7 @@ function Layout() {
  
                 </div>
                 
-                {/*<Footer scrollRef={scrollRef} marginTop={marginTop}></Footer>*/}
+                <Footer scrollRef={scrollRef} marginTop={marginTop}></Footer>
                 
             </div>
         </>
